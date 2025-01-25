c7a11b6cac6c27dbb241badf8f50b4135e23a5ad
AengChoon <kevin1131@naver.com>
2025-01-25 21:21:10 +0900

    RepositoryContext 추가

---
+++ CompanyEmployees.Infrastructure.Persistence/RepositoryContext.cs
@@ -0,0 +1,14 @@
+﻿using CompanyEmployees.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyEmployees.Infrastructure.Persistence;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Company>? Companies { get; set; }
+    public DbSet<Employee>? Employees { get; set; }
+}
