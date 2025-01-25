81e94f7a3efffaec0b28895abc2173dade6f0cb9
AengChoon <kevin1131@naver.com>
2025-01-25 21:34:50 +0900

    Add Migration

--- CompanyEmployees.Infrastructure.Persistence/Migrations/20250125123240_InitialData.Designer.cs
+++ CompanyEmployees.Infrastructure.Persistence/Migrations/20250125123240_InitialData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CompanyEmployees.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CompanyEmployees.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20250125123240_InitialData")]
+    partial class InitialData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.1");
